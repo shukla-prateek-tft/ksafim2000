116efe372070075ae90898c1293d735cc7dd24f9
Mohit Kesharwani <kesharwani.mohit@tftus.com>
2025-11-11 17:38:46 +0530

    Added the date validations in 0601

--- src/pages/pettyCash/pettyCashExpenses/PettyCashExpenses.controller.tsx
+++ src/pages/pettyCash/pettyCashExpenses/PettyCashExpenses.controller.tsx
@@ -1,12 +1,22 @@
 // MCFW-0601
-import React from 'react';
+import React, { useState } from 'react';
 import PettyCashExpensesUI from './PettyCashExpenses.render';
 import { GlobalLoader } from '@/components';
 import { useApiQuery, useAuth } from '@/hooks';
 import { adminServices } from '@/services';
 import { ServiceFn } from '../type';
 import { useEffect } from 'react';
-import { showToastErrors } from '@/utils/commonHelper';
+import {
+  convertPrevYearAug31,
+  getDateOnly,
+  getInstiYearAug31,
+  getKalendarYearDateRange,
+  getPrevYearAugust15,
+  getPrevYearSeptemberFirst,
+  gp_check_date,
+  isFutureDate,
+  showToastErrors
+} from '@/utils/commonHelper';
 import {
   GetAccCardByServiceTypeApiResponse,
   GetCutPettyCashExpensesAPIResponse,
@@ -15,10 +25,14 @@ import {
   GetServiceTypeListApiResponse,
   GetServiceTypeListFilterType,
   insertCutPettyCashExpenseParamType,
-  InsertCutPettyCashExpensesAPIResponse
+  InsertCutPettyCashExpensesAPIResponse,
+  TransactionDetailsType
 } from './types';
 import { GetAccCardsApiResponse } from '../invoice/enterInvoices/types';
 import { toast } from 'react-toastify';
+import { useTranslation } from 'react-i18next';
+import { RxCrossCircled } from 'react-icons/rx';
+import { useFocusFirstInvalidField } from '@/hooks/useFocusFirstInvalidField';
 
 export const defaultFilterValues: Partial<GetServiceTypeListFilterType> = {
   Page: 1,
@@ -30,11 +44,17 @@ export const defaultFilterValues: Partial<GetServiceTypeListFilterType> = {
 };
 
 const PettyCashExpenses = () => {
-  const { user } = useAuth();
+  const { user, toggleFlags } = useAuth();
+  const { focusFirstInvalidField } = useFocusFirstInvalidField();
+  const { t } = useTranslation('common');
   let serviceTypeOptions: Array<{ label: string; value: number }> = [];
   let projectNoListOptions: Array<{ label: string | number; value: number | string }> = [];
   let acc_CardOptions: Array<{ label: string | number; value: number | string }> = [];
-
+  const [transactionDetails, setTransactionDetails] = useState<TransactionDetailsType>({
+    supp: null,
+    date_Aut: new Date()
+  });
+  const [dateAutFlag, setDateAutFlag] = useState(false);
   // GetServiceTypeByAccCard
   const {
     state: {
@@ -130,12 +150,12 @@ const PettyCashExpenses = () => {
       Project_No: 201
     };
 
-    getServiceTypeLists(getServiceTypeParams);
-    getGetProjectNoLists(getServiceTypeParams);
-    getCutPettyCashExpenses(PettyCashDummyData);
-    getBankListServices(getServiceTypeParams);
-    getAccCardByServiceType(AccCardByServiceTypeDummyData);
-    getServiceTypeByAccCard(GetServiceTypeByAccCardDummyData);
+    // getServiceTypeLists(getServiceTypeParams);
+    // getGetProjectNoLists(getServiceTypeParams);
+    // getCutPettyCashExpenses(PettyCashDummyData);
+    // getBankListServices(getServiceTypeParams);
+    // getAccCardByServiceType(AccCardByServiceTypeDummyData);
+    // getServiceTypeByAccCard(GetServiceTypeByAccCardDummyData);
   }, []);
 
   const {
@@ -193,6 +213,10 @@ const PettyCashExpenses = () => {
   ]);
 
   const handleSave = () => {
+    setDateAutFlag(true);
+    if (transactionDetails?.date_Aut && validateTransactionDate(transactionDetails?.date_Aut)) {
+      return;
+    }
     const finalPayload: insertCutPettyCashExpenseParamType = {
       systemYear: 2025,
       institutionCode: 244111,
@@ -213,7 +237,7 @@ const PettyCashExpenses = () => {
       ]
     };
 
-    insertCutPettyCashExpenseSerivce(finalPayload);
+    // insertCutPettyCashExpenseSerivce(finalPayload);
   };
 
   serviceTypeOptions = getServiceTypeListsData?.data?.map(item => ({
@@ -232,6 +256,188 @@ const PettyCashExpenses = () => {
       value: item.accCard
     })) ?? [];
 
+  const handleDateChange = (date: Date | string | null, fieldName: string) => {
+    setDateAutFlag(true);
+    setTransactionDetails(prev => ({
+      ...prev,
+      [fieldName]: date ? date : null
+    }));
+  };
+
+  const validateTransactionDate = (transactionDate: string | Date) => {
+    const normalizeSelectDate = getDateOnly(new Date(transactionDate));
+    //dummy api data
+    const user2 = {
+      instiYear: '2025',
+      instiCode: '244111',
+      kalendarYear: '2025',
+      account_year_to: '2026-03-23'
+    };
+    const get_finance_year_Responce = 1;
+    const get_hoz_kupa_ktana_card = '';
+
+    if (normalizeSelectDate) {
+      if (get_finance_year_Responce == 1) {
+        const { kalendarYearStartDate, kalendarYearEndDate } = getKalendarYearDateRange(
+          user2.kalendarYear
+        );
+
+        // 1/jan/kalendarYear - selected date must be - 31/12/kalendarYear
+        if (
+          !normalizeSelectDate ||
+          normalizeSelectDate < kalendarYearStartDate ||
+          normalizeSelectDate > kalendarYearEndDate
+        ) {
+          console.log('error-1');
+          toggleFlags({
+            showValidationError: true,
+            errorData: {
+              message: `${t(
+                'validationErrorForKalenderYear'
+              )} ${`${t('yearStartDate')}${user.instiYear} - ${t('yearEndDate')}${user.instiYear} `}`,
+              dialogTitle: `${t('errorTitle')}-${user.hebrewYear}`,
+              confirmText: t('confirmText'),
+              confirmCallback: () => focusFirstInvalidField('date_Aut')
+            }
+          });
+          return true; // Validation failed
+        }
+      } else {
+        const { d1, d2 } = gp_check_date(user2.account_year_to);
+
+        // d2 - date must be - d1
+        if (!normalizeSelectDate || normalizeSelectDate > d1 || normalizeSelectDate < d2) {
+          //error- message $text(E_119)
+          console.log('error-2');
+          toggleFlags({
+            showValidationError: true,
+            errorData: {
+              message: `${t(
+                'validationErrorForKalenderYear'
+              )} ${`${t('yearStartDate')}${user.instiYear} - ${t('yearEndDate')}${user.instiYear} `}`,
+              dialogTitle: `${t('errorTitle')}-${user.hebrewYear}`,
+              confirmText: t('confirmText'),
+              confirmCallback: () => focusFirstInvalidField('date_Aut')
+            }
+          });
+          // return true;
+          //ange badhne do
+        }
+      }
+
+      if (get_hoz_kupa_ktana_card != '' || get_hoz_kupa_ktana_card != 0) {
+        const oneSepPrevYear = getPrevYearSeptemberFirst(user2.instiYear);
+
+        // date must be grater than 1/sep/prev year
+        if (!normalizeSelectDate || normalizeSelectDate < oneSepPrevYear) {
+          //error message `Date must be grater than ${oneSepPrevYear.toLocaleDateString()}`
+          //block
+          console.log('error-3');
+          toggleFlags({
+            showValidationError: true,
+            errorData: {
+              message: `${t(
+                'validationErrorForKalenderYear'
+              )} ${`${t('yearStartDate')}${user.instiYear} - ${t('yearEndDate')}${user.instiYear} `}`,
+              dialogTitle: `${t('errorTitle')}-${user.hebrewYear}`,
+              confirmText: t('confirmText'),
+              confirmCallback: () => focusFirstInvalidField('date_Aut')
+            }
+          });
+          return true; // Validation failed
+        }
+      } else {
+        const fifteenAugustPreYear = getPrevYearAugust15(user2.instiYear);
+        // date must be grater than 15/aug/prev year
+        if (!normalizeSelectDate || normalizeSelectDate < fifteenAugustPreYear) {
+          //error message `Date must be grater than ${fifteenAugustPreYear.toLocaleDateString()}`
+          console.log('error-4');
+          toggleFlags({
+            showValidationError: true,
+            errorData: {
+              message: `${t(
+                'validationErrorForKalenderYear'
+              )} ${`${t('yearStartDate')}${user.instiYear} - ${t('yearEndDate')}${user.instiYear} `}`,
+              dialogTitle: `${t('errorTitle')}-${user.hebrewYear}`,
+              confirmText: t('confirmText'),
+              confirmCallback: () => focusFirstInvalidField('date_Aut')
+            }
+          });
+          return true; // Validation failed //block
+        }
+      }
+
+      const { d1, d2 } = gp_check_date(user2.account_year_to);
+
+      // d2 - date must be - d1
+      if (!normalizeSelectDate || normalizeSelectDate > d1 || normalizeSelectDate < d2) {
+        //error- message $text(E_119)
+        console.log('error-5');
+        toggleFlags({
+          showValidationError: true,
+          errorData: {
+            message: `${t(
+              'validationErrorForKalenderYear'
+            )} ${`${t('yearStartDate')}${user.instiYear} - ${t('yearEndDate')}${user.instiYear} `}`,
+            dialogTitle: `${t('errorTitle')}-${user.hebrewYear}`,
+            confirmText: t('confirmText'),
+            confirmCallback: () => focusFirstInvalidField('date_Aut')
+          }
+        });
+        // return true;
+        //ange badhne do
+      }
+
+      const instiYearAug31 = getInstiYearAug31(user2?.instiYear ?? '');
+      // future date not accepted
+      if (isFutureDate(normalizeSelectDate)) {
+        // error message `Date cannot be Greater than ${tomorrowDate.toLocaleDateString()}`
+        //block
+        console.log('error-6');
+        toggleFlags({
+          showValidationError: true,
+          errorData: {
+            message: `${t(
+              'validationErrorForKalenderYear'
+            )} ${`${t('yearStartDate')}${user.instiYear} - ${t('yearEndDate')}${user.instiYear} `}`,
+            dialogTitle: `${t('errorTitle')}-${user.hebrewYear}`,
+            confirmText: t('confirmText'),
+            confirmCallback: () => focusFirstInvalidField('date_Aut')
+          }
+        });
+        return true; // Validation failed
+      } else if (normalizeSelectDate && normalizeSelectDate > instiYearAug31) {
+        console.log('error-7');
+        // last date must be less than insti year aug 31
+        toggleFlags({
+          showValidationError: true,
+          errorData: {
+            message: `${t(
+              'validationErrorForKalenderYear'
+            )} ${`${t('yearStartDate')}${user.instiYear} - ${t('yearEndDate')}${user.instiYear} `}`,
+            dialogTitle: `${t('errorTitle')}-${user.hebrewYear}`,
+            confirmText: t('confirmText'),
+            confirmCallback: () => focusFirstInvalidField('date_Aut')
+          }
+        });
+        return true; // Validation failed // block
+      }
+    }
+    return false; // Validation passed
+  };
+  useEffect(() => {
+    if (dateAutFlag) {
+      if (transactionDetails?.date_Aut && validateTransactionDate(transactionDetails?.date_Aut)) {
+        return;
+      }
+    }
+  }, [transactionDetails.date_Aut]);
+
+  const handleValidation = () => {
+    if (validateTransactionDate(transactionDetails?.date_Aut ?? '')) {
+      return;
+    }
+  };
   return (
     <>
       <GlobalLoader
@@ -253,6 +459,9 @@ const PettyCashExpenses = () => {
         acc_CardOptions={acc_CardOptions}
         getCutPettyCashExpensesData={getCutPettyCashExpensesData}
         handleSave={handleSave}
+        transactionDetails={transactionDetails}
+        handleDateChange={handleDateChange}
+        handleValidation={handleValidation}
       />
     </>
   );
